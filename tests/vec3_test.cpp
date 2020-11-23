405bcd2497be967b046ad051a3bddfdfe684cf19
Jordan-Lane <jlane@ualberta.ca>
2020-11-23 13:47:32 -0800

    Rework CMake to properly support testing

--- tests/vec3_test.cpp
+++ tests/vec3_test.cpp
@@ -1,5 +1,5 @@
 #include "gtest/gtest.h"
-#include "../src/common/vec3.h"
+#include "vec3.h"
 
 // GTest Primer: https://github.com/google/googletest/blob/master/googletest/docs/primer.md
 
@@ -8,7 +8,7 @@ class Vec3Test: public ::testing::Test {
 };
 
 TEST_F(Vec3Test, GeneralTest){
-
+    
 }
 
 
